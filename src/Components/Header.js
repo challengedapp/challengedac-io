0ef097c5555d4ba57f65b1f2fa217d989f64cdcb
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-11-21 20:22:55 +0530

    add minor things

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -96,7 +96,7 @@ function Header(props) {
                           className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
                         >
                           {" "}
-                          <NavLink to="/gettingstarted">
+                          <NavLink to="/gettingstart">
                             Gettig Started
                           </NavLink>{" "}
                         </p>
@@ -105,7 +105,7 @@ function Header(props) {
                           className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
                         >
                           {" "}
-                          <NavLink to="/deposits">
+                          <NavLink to="/deposite">
                             {" "}
                             How Deposit Funds
                           </NavLink>{" "}
@@ -181,7 +181,7 @@ function Header(props) {
                           className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
                         >
                           {" "}
-                          <NavLink to="/gettingstarted">
+                          <NavLink to="/gettingstart">
                             Gettig Started
                           </NavLink>{" "}
                         </p>
@@ -190,7 +190,7 @@ function Header(props) {
                           className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
                         >
                           {" "}
-                          <NavLink to="/deposits">
+                          <NavLink to="/deposite">
                             {" "}
                             How Deposit Funds
                           </NavLink>{" "}
